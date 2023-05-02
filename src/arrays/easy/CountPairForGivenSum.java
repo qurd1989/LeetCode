f4b40196b9eaf7344a6438b4888583ce1c70c015
Qurd1989 <mashaedu@gmail.com>
2023-05-01 23:06:52 -0400

    new change

--- src/arrays/easy/CountPairForGivenSum.java
+++ src/arrays/easy/CountPairForGivenSum.java
@@ -1,7 +1,9 @@
 package arrays.easy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class CountPairForGivenSum {
 
@@ -12,28 +14,49 @@ public class CountPairForGivenSum {
 
         int arr[] = { 1, 4, 45, 6, 10, 8, 8, 17, 16, 15};
         int sum = 16;
-        int[] result = solve(arr, sum);
-        System.out.println(Arrays.toString(result));
+        int[][] result = solve(arr, sum);
+        for (int[] pair : result) {
+            System.out.println(Arrays.toString(pair));
+        }
+
+
+        int[] array = new int[6];
+
+// Current length is 0, because it has 0 elements.
+        int length = 0;
+
+// Add 3 items into it.
+        for (int i = 0; i < 3; i++) {
+            array[i] = i * i;
+            // Each time we add an element, the length goes up by one.
+            length++;
+        }
+
+        System.out.println("The Array has a capacity of " + array.length);
+        System.out.println("The Array has a length of " + length);
     }
 
-    public  static int[] solve(int arr[], int sum) {
+    public  static int[][] solve(int arr[], int sum) {
 
         int n = arr.length;
 
         HashSet<Integer>hs = new HashSet<>();
-        int count = 0;
-        for (int i =0; i < n; i++) {
+        List<int[]> pairs = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
             int j = sum - arr[i];
             if (hs.contains(j)) {
-                count++;
-                System.out.println(count);
+                int[] pair = {j, arr[i]};
+                pairs.add(pair);
             }
             hs.add(arr[i]);
+
         }
-        int result[] = new int[hs.size()];
+        int[][] result = new int[pairs.size()][2];
         int i = 0;
-        for (Integer num : hs) {
-            result[i++] = num;
+
+        for (int[] pair : pairs) {
+            result[i++] = pair;
 
         }
         return result;
