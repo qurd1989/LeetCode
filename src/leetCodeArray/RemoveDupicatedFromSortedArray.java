1c0bac478c7ab165461bdce6dcd37b7ac266908f
Qurd1989 <mashaedu@gmail.com>
2024-04-07 23:42:40 -0400

    new change

--- src/leetCodeArray/RemoveDupicatedFromSortedArray.java
+++ src/leetCodeArray/RemoveDupicatedFromSortedArray.java
@@ -1,19 +1,29 @@
 package leetCodeArray;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class RemoveDupicatedFromSortedArray {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n-1 ; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(removeDuplicates(arr));
     }
-    public int removeDuplicates(int[] nums) {
-        int n =nums.length;
-        if (n == 0){
+    public static int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
             return 0;
         }
         int addIndexCurrentArray = 0;
 
-        for (int i = 0; i < n; i++){
-            if (nums[i] < nums[i+1]){
-                nums[addIndexCurrentArray] = nums[i+1];
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != nums[addIndexCurrentArray]) {
+                nums[addIndexCurrentArray] = nums[i];
                 addIndexCurrentArray++;
             }
         }
