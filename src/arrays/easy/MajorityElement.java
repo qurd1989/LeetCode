a9aa1a6b64b1020bbf5bbae4fcd9b4c48eaa4b7b
Qurd1989 <mashaedu@gmail.com>
2024-05-04 22:51:33 -0400

    new change

--- src/arrays/easy/MajorityElement.java
+++ src/arrays/easy/MajorityElement.java
@@ -2,21 +2,32 @@ package arrays.easy;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class MajorityElement {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String subString = s.substring(3, 6);
+        int lastThreeDigits = Integer.valueOf(subString);
+        System.out.println(subString);
+        int v = lastThreeDigits/3;
+        System.out.println(lastThreeDigits);
+        if (lastThreeDigits >= 1 &&  lastThreeDigits <= 349 || lastThreeDigits != 316) {
+            System.out.println("Yes");
+        } else
+            System.out.println("No");
     }
-    public int majorityElement(int prices[]){
-        int buy = prices[0];
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++){
-            if (prices[i] < buy){
-                buy = prices[i];
-            }else if (prices[i] - buy > profit ){
-                profit = prices[i] - buy;
-            }
-        }
-        return profit;
-    }
+//    public int majorityElement(int prices[]){
+//        int buy = prices[0];
+//        int profit = 0;
+//        for (int i = 1; i < prices.length; i++){
+//            if (prices[i] < buy){
+//                buy = prices[i];
+//            }else if (prices[i] - buy > profit ){
+//                profit = prices[i] - buy;
+//            }
+//        }
+//        return profit;
+//
 }
