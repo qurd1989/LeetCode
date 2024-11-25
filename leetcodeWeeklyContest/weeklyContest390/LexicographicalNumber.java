eec95c080950ed0e172e9a8ef659a576903d2a62
Qurd1989 <mashaedu@gmail.com>
2024-11-24 22:31:36 -0500

    new change

--- leetcodeWeeklyContest/weeklyContest390/LexicographicalNumber.java
+++ leetcodeWeeklyContest/weeklyContest390/LexicographicalNumber.java
@@ -1,7 +1,26 @@
 package weeklyContest390;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LexicographicalNumber {
     public static void main(String[] args) {
 
+    }
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> lexicographicalNumber = new ArrayList<>();
+        int currentNum = 1;
+        for (int i = 1; i <= 9; i++){
+            lexicographicalNumber.add(currentNum);
+            if (currentNum * 10 <= n){
+                currentNum *= 10;
+        }else {
+                while (currentNum % 10 == 9 || currentNum == n){
+                    currentNum /= 10;
+                }
+                currentNum++;
+            }
+        }
+        return lexicographicalNumber;
     }
 }
