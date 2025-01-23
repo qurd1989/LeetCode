f170516ab8b3edfdf165bfff8269590be5423322
Qurd1989 <mashaedu@gmail.com>
2025-01-22 23:38:52 -0500

    new change

--- leetcodeWeeklyContest/weeklyContest390/MaximizeScoreOfNumbersInRanges.java
+++ leetcodeWeeklyContest/weeklyContest390/MaximizeScoreOfNumbersInRanges.java
@@ -1,5 +1,15 @@
 package weeklyContest390;
 
 public class MaximizeScoreOfNumbersInRanges {
+    public static void main(String[] args) {
+        MaximizeScoreOfNumbersInRanges maximizeScoreOfNumbersInRanges = new MaximizeScoreOfNumbersInRanges();
+        int[] nums = {1, 2, 3, 4, 5};
+        int[][] requests = {{1, 3}, {0, 1}};
+        System.out.println(maximizeScoreOfNumbersInRanges.maximizeScore(nums, requests));
+    }
 
+    private boolean maximizeScore(int[] nums, int[][] requests) {
+
+        return false;
+    }
 }
