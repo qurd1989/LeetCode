79b4bab397801a7042b1d1efe98c219ba29b61e3
Qurd1989 <mashaedu@gmail.com>
2025-04-01 01:22:01 -0400

    Longest consecutive 1s

--- src/arrays/MaxConsecutiveOnes.java
+++ src/arrays/MaxConsecutiveOnes.java
@@ -3,7 +3,7 @@ package arrays;
 public class MaxConsecutiveOnes {
     public static void main(String[] args) {
         int arr[] = {1,0,1,0,1,1,1,1};
-        System.out.println(findMaxConsecutiveOnes(arr));
+        System.out.println(findMaxConsecutiveOnesTwo(arr));
     }
     public static int findMaxConsecutiveOnes(int[] nums) {
         int max = 0;
@@ -15,4 +15,23 @@ public class MaxConsecutiveOnes {
         }
         return max;
     }
+    public static int findMaxConsecutiveOnesTwo(int[] nums) {
+        int max = 0;
+        int currOnes = 0, prevOnes = 0, seenZero = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                seenZero = 1;
+                prevOnes = currOnes;
+                currOnes = 0;
+            } else {
+                currOnes++;
+            }
+
+            max = Math.max(max, currOnes + prevOnes + seenZero);
+        }
+
+        return max;
+    }
+
 }
